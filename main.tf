a8e4c7b48d465f196e9b4eb709c7b30eb5c899e2
Rajiv Shakya <rajivshaky@gmail.com>
2026-04-07 13:03:23 +0530

    tfsec issue resolution

--- main.tf
+++ main.tf
@@ -4,6 +4,9 @@ provider "aws" {
 resource "aws_instance" "ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  metadata_options {
+    http_tokens = "required"
+  }
   root_block_device {
     encrypted = true
   }
