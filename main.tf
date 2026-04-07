13eaf0e95fd71dfe8203ab6d6d0a8ef00bbc8357
Rajiv Shakya <rajivshaky@gmail.com>
2026-04-07 12:42:08 +0530

    terrrm file fmt done

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.aws_region
 }
 resource "aws_instance" "ec2" {
-  ami = var.ami_id
+  ami           = var.ami_id
   instance_type = var.instance_type
   tags = {
     Name = "App-Server"
